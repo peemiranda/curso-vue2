7e22719e52fb3bfe5abdb2b7065b44d0edce10d7
Pedro Henrique Miranda de Oliveira <pedro.henrique@metadil.com.br>
2024-02-06 16:01:14 -0300

    js pessoas componentes em detalhes

---
+++ aula-25/pessoas.js
@@ -0,0 +1,65 @@
+const pessoas = [
+    {
+        name: "pedro",
+        email: "pedro.henrique@metadil.com.br",
+        phone: "11999999999",
+        date: "2020-04-08T12:00:00-03:00",
+        balance: 100.10
+    },
+    {
+        name: "Isa",
+        email: "Isa@gmail.com",
+        phone: "11999999912",
+        date: "2021-02-19T12:00:00-03:00",
+        balance: 100.10
+    },
+    {
+        name: "Caio",
+        email: "Caio@gmail.com",
+        phone: "11999999913",
+        date: "2020-03-08T12:00:00-03:00",
+        balance: 100.10
+    },
+    {
+        name: "Nicolas",
+        email: "Nicolas@gmail.com",
+        phone: "11999999914",
+        date: "2021-04-02T12:00:00-03:00",
+        balance: 100.10
+    },
+    {
+        name: "Luiz",
+        email: "Luiz@gmail.com",
+        phone: "11999999915",
+        date: "2023-04-08T12:00:00-03:00",
+        balance: 100.10
+    },
+    {
+        name: "Alisson",
+        email: "Alisson@gmail.com",
+        phone: "11999999916",
+        date: "2020-04-28T12:00:00-03:00",
+        balance: 100.10
+    },
+    {
+        name: "Morishita",
+        email: "Morishita@gmail.com",
+        phone: "11999999917",
+        date: "2022-04-08T12:00:00-03:00",
+        balance: 100.10
+    },
+    {
+        name: "Gabriel",
+        email: "Gabriel@gmail.com",
+        phone: "11999999918",
+        date: "2020-02-08T12:00:00-03:00",
+        balance: 100.10
+    },
+    {
+        name: "diguinho",
+        email: "diguinho@gmail.com",
+        phone: "11999999919",
+        date: "2021-04-08T12:00:00-03:00",
+        balance: 100.10
+    }
+]
